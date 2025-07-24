c26af2b37a90d3693af7119f887d7fda1240ecc0
user <user@gmail.com>
2025-07-24 14:04:23 +0300

    feat(exceptions): Add custom exceptions
    - Introduce `MAX_FILE_SIZE` and `SUPPORTED_FORMATS` settings for file upload validation.
    - Add custom exception classes to handle errors for unsupported formats, size limits, and multiple file uploads, improving error clarity and management.

--- services/backend/src/settings/config.py
+++ services/backend/src/settings/config.py
@@ -17,6 +17,9 @@ class AppConfig(BaseSettings):
     WEB_SERVER_WORKERS: int
     WEB_SERVER_START_PORT: int
 
+    MAX_FILE_SIZE: int = 5 * 1024 * 1024
+    SUPPORTED_FORMATS: set[str] = {'.jpg', '.png', '.gif'}
+
     model_config = SettingsConfigDict(
         env_file=str(BASE_DIR / "backend/.env"),
         env_file_encoding="utf-8"
